f12908ce71b91496b10a6bb2b2f114315b130bba
vikram singh <58397405+Sparta-9@users.noreply.github.com>
2022-10-29 19:04:15 +0530

    Update List.py
    indentation error

--- List.py
+++ List.py
@@ -1,15 +1,11 @@
-class Myclass :
+class Myclass:
     def __init__(self,a,b):
         self.num1=a
         self.num2=b
-
     def add(self):
         self.sum=self.num1+self.num2
-
-    def show(self) :
+    def show(self):
         print(self.sum)
-
-
 a=Myclass(5,3)
 a.add()
 a.show()
@@ -17,8 +13,6 @@ a.num1=90
 a.num2=90
 a.add()
 a.show()
-
-
 print(a.add())
 a.show()
 
